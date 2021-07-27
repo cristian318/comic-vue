1c658aecd5f0c827230d46613a358497ba212c02
Cristian Esteban Garzon Lopez <cristian.garzon@masivian.com>
2021-07-27 18:56:44 -0500

    Se crea el componente StarRating

---
+++ src/components/StarRating.vue
@@ -0,0 +1,51 @@
+<template>
+  <div @mouseleave="onMouseLeave" class="star-rating">
+    <span v-for="n in 5" :key="n" @mouseover="onMouseOver(n)" class="star-rating__star">
+      <img :src="getImage(n)" alt="star" @click="selectStar(n)" />
+    </span>
+  </div>
+</template>
+
+<script>
+import ImageStar from '@/assets/img/icons/star.svg';
+import ImageStarFill from '@/assets/img/icons/star-fill.svg';
+
+export default {
+  props: {
+    stars: {
+      type: Number,
+      default: () => 5,
+    },
+    value: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  data() {
+    return {
+      star: 0,
+    };
+  },
+  methods: {
+    selectStar(star) {
+      this.star = star;
+      this.$emit('input', star);
+    },
+    onMouseOver(star) {
+      this.star = star;
+    },
+    onMouseLeave() {
+      this.star = this.value;
+    },
+    getImage(star) {
+      let srcImage;
+      if (star > this.star) {
+        srcImage = ImageStar;
+      } else {
+        srcImage = ImageStarFill;
+      }
+      return srcImage;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <StarRating v-model="starModel" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import StarRating from '@/components/StarRating.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    StarRating,
+  },
+  data() {
+    return {
+      starModel: 0,
+    };
   },
 };
 </script>
