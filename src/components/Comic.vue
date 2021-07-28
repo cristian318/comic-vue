a3ab4957780a115812d1a0dbd708e7a30680ddeb
Cristian Esteban Garzon Lopez <cristian.garzon@masivian.com>
2021-07-27 19:18:40 -0500

    Se ajusta la ruta y base del componente Comic

---
+++ src/components/Comic.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <StarRating v-model="starModel" />
+  </div>
+</template>
+
+<script>
+import StarRating from '@/components/StarRating.vue';
+
+export default {
+  name: 'Comic',
+  components: {
+    StarRating,
+  },
+  data() {
+    return {
+      starModel: 0,
+    };
+  },
+  methods: {
+    validateRoute() {
+      if (this.$route?.params?.comic) {
+        console.log('Comic route', this.$route.params.comic);
+      }
+    },
+  },
+  mounted() {
+    this.validateRoute();
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <StarRating v-model="starModel" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Comic />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-import StarRating from '@/components/StarRating.vue';
+import Comic from '@/components/Comic.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-    StarRating,
-  },
-  data() {
-    return {
-      starModel: 0,
-    };
+    Comic,
   },
 };
 </script>
